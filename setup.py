f6e092b095fc93eaff95e2f52ee585c0d492d7be
Alfaroque Islam <alfaroque5@gmail.com>
2020-07-07 11:31:27 -0400

    updated setup file

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 setuptools.setup(
-    name="lambdata_alfaroqueIslam",
-    version="0.0.2",
+    name="lambdata-alfaroqueIslam",
+    version="0.0.1",
     author="Alfaroque Islam",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
